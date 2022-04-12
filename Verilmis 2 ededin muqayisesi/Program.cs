1462fd58fc96ba1a26d1023d87a7f24cc3fe1bc9
[ismaylibra] <[ismayiliib@code.edu.az]>
2022-04-13 01:27:50 +0400

    created c sharp file (homework)

---
+++ Verilmis 2 ededin muqayisesi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Verilmis_2_ededin_muqayisesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Birinci ededi daxil edin:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ikinci ededi daxil edin:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1>num2)
+            {
+                Console.WriteLine(num1 + " " + "boyukdur");
+            }
+            else if (num1<num2)
+            {
+                Console.WriteLine(num2 + " " + " boyukdur" );
+            }
+            else
+            {
+                Console.WriteLine("Verilmis ededler beraberdir");
+            }
+        }
+    }
+}
